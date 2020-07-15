d4abf9cb2653eaa75cd7db309a851bb2304cee93
dengzhijie <1104397829@qq.com>
2020-07-15 15:09:20 +0800

    random forest

--- MeachineLearning/random_forecast.py
+++ MeachineLearning/random_forecast.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-# @File: decisionTree.py
+# @File: random_forecast.py
 # @Author: dengzj
 # @E-mail: 1104397829@qq.com
 
+#以分类决策树作为弱分类器
+
+import numpy as np
 #ID3分类树
 import math
 #计算给定数据集的香农熵值
@@ -114,22 +117,91 @@ def create_decision_tree(dataset,labels):
         res_decision_tree[label_best][value]=create_decision_tree(split_dataset(dataset,best_feature,value),sub_labels)
 
     return res_decision_tree
+# 参数意思分别 是从a 中以概率P，随机选择3个, p没有指定的时候相当于是一致的分布，a为一维数组或int，如果是int，则生成随机样本，就好像a是np.arange(N)一样。
+# a1 = np.random.choice(a=5, size=3, replace=False, p=None)
+# 非一致的分布，会以多少的概率提出来
+# a2 = np.random.choice(a=5, size=3, replace=False, p=[0.2, 0.1, 0.3, 0.4, 0.0])
+# replacement 代表的意思是抽样之后还放不放回去，如果是False的话，那么出来的三个数都不一样，如果是
+# True的话， 有可能会出现重复的，因为前面的抽的放回去了。
 
+#print(np.random.choice(['a','b','c','d'],3,replace=True))
+#dataset为数据，包括特征数据和标签。labels为特征名称的集合
+def model_rf(dataset,labels,n_trees):
+    trees=[]
+    for i in range(n_trees):
+        nums_datas,nums_features=np.array(dataset).shape
+        nums_features-=1
+        #随机选取数据和特征
+        idxs_d = np.random.choice(nums_datas, nums_datas-1, replace=True)
+        idxs_f = np.random.choice(nums_features, nums_features-1, replace=False)
+        #print(idxs_d)
+        #print(idxs_f)
+        #np转list
+        labels_list = labels[idxs_f].tolist()
+        idxs_fl=np.append(idxs_f,-1)
+        #print(dataset[idxs_d][:,idxs_fl])
+        #a=dataset[idxs_d].tolist()
+        dataset_list=dataset[idxs_d][:,idxs_fl].tolist()
+        #labels_list=labels[idxs_f]
+        #print('qq',dataset_list,labels_list)
+        trees.append(create_decision_tree(dataset_list,labels_list))
+    return trees
+#单条数据的预测
+def predict_rf(model,data_predict):
+    res=[]
+    for i in range(len(model)):
+        if isinstance(model[i],str):
+            res.append(model[i])
+            continue
+        # print(model[i].keys())
+        # print(model[i][list(model[i].keys())[0]])
+        value=model[i][list(model[i].keys())[0]]
+        dict_index={'no surfing':0,'flippers':1,'others':2}
+        # print(list(value.keys()))
+        # print(value)
+        temp=model[i]
 
+        print(value)
+        print(list(temp.keys())[0])
+        print(dict_index[list(temp.keys())[0]])
+        print(data_predict[dict_index[list(temp.keys())[0]]])
+        print(value[str(data_predict[dict_index[list(temp.keys())[0]]])])
+        #print(dict_index[list(model[i].keys())[0]])
+        temp_value=value[str(data_predict[dict_index[list(temp.keys())[0]]])]
+        #从模型存储的字典中遍历到最终的预测结果
+        while temp_value not in ['yes','no']:
+            value=temp_value[list(temp_value.keys())[0]]
+            feature_value=data_predict[dict_index[list(temp_value.keys())[0]]]
+            temp_value=value[str(feature_value)]
+        res.append(temp_value)
+
+    #返回列表中个数最多的元素，即为随机森林最终预测的标签
+    return max(res, key=res.count)
 
 if __name__=='__main__':
-    #dataset 由特征加标签组成
-    data_test=[
-        [1, 1, 'yes'],
-        [1, 1, 'yes'],
-        [1, 0, 'no'],
-        [0, 1, 'no'],
-        [0, 1, 'no']
+    #测试数据选取不当可能会存在报错，原因是随机选取数据时会导致某个特征不能完全包括所有的可能值，由此导致错误
+    data_test = [
+        [1, 1, 1,'yes'],
+        [1, 1, 0,'yes'],
+        [1, 0, 0,'no'],
+        [0, 1, 0,'no'],
+        [0, 1, 0,'no'],
+        [1, 1, 1, 'yes'],
+        [1, 1, 0, 'yes'],
+        [1, 0, 0, 'no'],
+        [0, 1, 0, 'no'],
+        [0, 1, 0, 'no'],
+        [1, 1, 1, 'yes'],
+        [1, 1, 0, 'yes'],
+        [1, 0, 0, 'no'],
+        [0, 1, 0, 'no'],
+        [0, 1, 0, 'no']
     ]
-    labels=['no surfing','flippers']
-    print(cal_shannon_ent(data_test))
-    print(split_dataset(data_test,0,0))
-    list_label_i=[data[0] for data in data_test]
-    print(list_label_i)
-    print(choose_best_splitfeature(data_test))
-    print(create_decision_tree(data_test,labels))
+    labels = ['no surfing', 'flippers','others']
+    res=model_rf(np.array(data_test),np.array(labels),10)
+    #print(predict_rf(res,data_test))
+    print('res',res)
+    print(predict_rf(res,[1, 1, 1]))
+
+
+
